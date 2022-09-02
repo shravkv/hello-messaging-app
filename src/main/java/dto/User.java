b98f4bf81c39df6f30daaca5ba8a312ee99fb7a4
shravkv <sravan.k.vodnala@gmail.com>
2022-09-02 11:05:44 +0530

    Added: Use get request method to demonstrate rest API

---
+++ src/main/java/dto/User.java
@@ -0,0 +1,32 @@
+package dto;
+
+public class User {
+
+    String firstName;
+    String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+}
