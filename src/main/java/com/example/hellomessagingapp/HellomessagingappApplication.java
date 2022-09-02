e8fe8eff431b5c4bbe76b153f93a6259e7ab02fe
shravkv <sravan.k.vodnala@gmail.com>
2022-09-02 10:31:06 +0530

    Added: Hello messaging app

---
+++ src/main/java/com/example/hellomessagingapp/HellomessagingappApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellomessagingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellomessagingappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellomessagingappApplication.class, args);
+    }
+
+}
