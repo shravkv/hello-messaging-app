13f6d7fc5343400b380fc6331487365a8eb09cc5
shravkv <sravan.k.vodnala@gmail.com>
2022-09-02 11:25:06 +0530

    Added: Use get request method and pass name as query parameter

--- src/main/java/dto/HelloMessagingAppController.java
+++ src/main/java/dto/HelloMessagingAppController.java
@@ -1,50 +1,22 @@
 package dto;
 
-import org.springframework.web.bind.annotation.*;
-import com.bridgelabz.hellomessagingapp.controller.dto.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
-
 public class HelloMessagingAppController {
+
     @GetMapping("/getdata")
     public String hello() {
         return "Hello Bridgelabz!";
     }
 
-
     @GetMapping("/query")
     public String querryParam(@RequestParam String name){
         return "Hello" + " " + name + " " + "from Bridgelabz";
-    }
-
-    @GetMapping("/path/{name}")
-    public String nameParam(@PathVariable String name) {
-        return "Hello" + " " + name + " " + "!";
-    }
-
-    @PostMapping("/adduser")
-    public String addUser(@RequestBody User user) {
-        return "Hello" + user.getFirstName() + " " + user.getLastName() +"!";
-    }
-
-
-    @PostMapping("/post")
-    public String addUserRequestParam(@RequestParam String firstName, @RequestParam String lastName) {
-        User user = new User();
-        user.setFirstName(firstName);
-        user.setLastName(lastName);
-        return user.toString();
-    }
-
-    @PutMapping("/updated")
-    public String update(@RequestParam String firstName, @RequestParam String lastName) {
-        User user = new User();
-        user.setFirstName("Vodnala");
-        user.setLastName("Sravan");
-        user.setFirstName(firstName);
-        user.setLastName(lastName);
-        return user.toString();
     }
 
 }
--- src/main/java/dto/HellomessagingappApplication.java
+++ src/main/java/dto/HellomessagingappApplication.java
@@ -1,4 +1,4 @@
-package com.example.hellomessagingapp;
+package dto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
